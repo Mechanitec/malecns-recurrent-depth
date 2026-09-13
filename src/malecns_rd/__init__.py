2cac125a0d32e6ac064296fdf276df481eedf86d
Mechanitec <129343009+Mechanitec@users.noreply.github.com>
2026-09-13 13:43:45 -0400

    Export LIF recurrent-depth API

--- src/malecns_rd/__init__.py
+++ src/malecns_rd/__init__.py
@@ -1,4 +1,11 @@
 from .graph import ConnectomeGraph
 from .engine import RecurrentDepthEngine, RunResult
+from .lif import LIFRecurrentDepthEngine, LIFRunResult
 
-__all__ = ["ConnectomeGraph", "RecurrentDepthEngine", "RunResult"]
+__all__ = [
+    "ConnectomeGraph",
+    "RecurrentDepthEngine",
+    "RunResult",
+    "LIFRecurrentDepthEngine",
+    "LIFRunResult",
+]
